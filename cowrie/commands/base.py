335ffcbd848bc004bf624849fcea447ebe0b0214
fe7ch <fetch06@hotmail.com>
2018-03-02 08:48:16 +0300

    Implement command_base64, fix bug in command_echo

--- cowrie/commands/base.py
+++ cowrie/commands/base.py
@@ -156,13 +156,14 @@ class command_echo(HoneyPotCommand):
                 s = s[:-2]
                 newline = False
 
+            if newline is True:
+                s += '\n'
+
             self.write(escape_fn(s))
 
         except ValueError as e:
             log.msg("echo command received Python incorrect hex escape")
 
-        if newline is True:
-            self.write('\n')
 
 commands['/bin/echo'] = command_echo
 
@@ -678,8 +679,137 @@ class command_chattr(HoneyPotCommand):
         if not self.fs.exists(self.args[1]):
             self.write('chattr: No such file or directory while trying to stat ' + self.args[1] + '\n')
         return
+
+
 commands['/usr/bin/chattr'] = command_chattr
 
+
+class command_base64(HoneyPotCommand):
+    """
+    author: Ivan Korolev (@fe7ch)
+    """
+    def start(self):
+        """
+        """
+
+        print('start')
+
+        self.mode = 'e'
+        self.ignore = False
+
+        if '--version' in self.args:
+            self.write(
+                """base64 (GNU coreutils) 8.21
+Copyright (C) 2013 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Written by Simon Josefsson.
+"""
+            )
+            self.exit()
+            return
+
+        if '--help' in self.args:
+            self.write(
+                """Usage: base64 [OPTION]... [FILE]
+Base64 encode or decode FILE, or standard input, to standard output.
+
+Mandatory arguments to long options are mandatory for short options too.
+  -d, --decode          decode data
+  -i, --ignore-garbage  when decoding, ignore non-alphabet characters
+  -w, --wrap=COLS       wrap encoded lines after COLS character (default 76).
+                          Use 0 to disable line wrapping
+
+      --help     display this help and exit
+      --version  output version information and exit
+
+With no FILE, or when FILE is -, read standard input.
+
+The data are encoded as described for the base64 alphabet in RFC 3548.
+When decoding, the input may contain newlines in addition to the bytes of
+the formal base64 alphabet.  Use --ignore-garbage to attempt to recover
+from any other non-alphabet bytes in the encoded stream.
+
+Report base64 bugs to bug-coreutils@gnu.org
+GNU coreutils home page: <http://www.gnu.org/software/coreutils/>
+General help using GNU software: <http://www.gnu.org/gethelp/>
+For complete documentation, run: info coreutils 'base64 invocation'
+"""
+            )
+            self.exit()
+            return
+
+        if '-d' in self.args:
+            self.mode = 'd'
+            self.args.remove('-d')
+
+        if '--decode' in self.args:
+            self.mode = 'd'
+            self.args.remove('--decode')
+
+        if '-i' in self.args:
+            self.ignore = True
+            self.args.remove('-i')
+
+        if '--ignore-garbage' in self.args:
+            self.ignore = True
+            self.args.remove('--ignore-garbage')
+
+        if '-w' in self.args:
+            self.args.remove(self.args[self.args.index('-w')+1])
+            self.args.remove('-w')
+
+        if self.input_data:
+            self.dojob(self.input_data)
+        else:
+            if len(self.args) > 1:
+                self.errorWrite(
+                    """base64: extra operand '%s'
+Try 'base64 --help' for more information.
+""" % self.args[0]
+                )
+                self.exit()
+                return
+
+            pname = self.fs.resolve_path(self.args[0], self.protocol.cwd)
+            if not self.fs.isdir(pname):
+                try:
+                    self.dojob(self.fs.file_contents(pname))
+                except Exception as e:
+                    print(str(e))
+                    self.errorWrite('base64: {}: No such file or directory\n'.format(self.args[0]))
+            else:
+                self.errorWrite('base64: read error: Is a directory\n')
+
+        self.exit()
+
+    def dojob(self, s):
+        if self.ignore:
+            s = ''.join([i for i in s if i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='])
+
+        if self.mode == 'e':
+            self.write(s.encode('base64'))
+        else:
+            try:
+                self.write(s.decode('base64'))
+            except:
+                self.errorWrite("base64: invalid input\n")
+
+    def lineReceived(self, line):
+        log.msg(eventid='cowrie.session.input',
+                realm='base64',
+                input=line,
+                format='INPUT (%(realm)s): %(input)s')
+
+        self.dojob(line)
+
+    def handle_CTRL_D(self):
+        self.exit()
+
+
+commands['/usr/bin/base64'] = command_base64
 
 
 class command_nop(HoneyPotCommand):
@@ -689,6 +819,8 @@ class command_nop(HoneyPotCommand):
         """
         """
         pass
+
+
 commands['umask'] = command_nop
 commands['set'] = command_nop
 commands['unset'] = command_nop
