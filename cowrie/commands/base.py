ce468043c97a1cb4830b6b7d9cd2ca005ca9c6d0
fe7ch <fetch06@hotmail.com>
2018-03-13 10:13:24 +0300

    command_base64:
        [!] Parse arguments with getopt

--- cowrie/commands/base.py
+++ cowrie/commands/base.py
@@ -692,35 +692,26 @@ class command_base64(HoneyPotCommand):
         """
         """
 
-        print('start')
-
         self.mode = 'e'
         self.ignore = False
 
-        if '--version' in self.args:
-            self.write(
-                """base64 (GNU coreutils) 8.21
-Copyright (C) 2013 Free Software Foundation, Inc.
-License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
-This is free software: you are free to change and redistribute it.
-There is NO WARRANTY, to the extent permitted by law.
-
-Written by Simon Josefsson.
-"""
-            )
+        try:
+            optlist, args = getopt.getopt(self.args, 'diw:', ['version', 'help', 'decode', 'ignore-garbage', 'wrap='])
+        except getopt.GetoptError as err:
+            self.errorWrite('Unrecognized option\n')
             self.exit()
             return
 
-        if '--help' in self.args:
-            self.write(
-                """Usage: base64 [OPTION]... [FILE]
+        for opt in optlist:
+            if opt[0] == '--help':
+                self.write("""Usage: base64 [OPTION]... [FILE]
 Base64 encode or decode FILE, or standard input, to standard output.
 
 Mandatory arguments to long options are mandatory for short options too.
   -d, --decode          decode data
   -i, --ignore-garbage  when decoding, ignore non-alphabet characters
   -w, --wrap=COLS       wrap encoded lines after COLS character (default 76).
-                          Use 0 to disable line wrapping
+                        Use 0 to disable line wrapping
 
       --help     display this help and exit
       --version  output version information and exit
@@ -736,50 +727,47 @@ Report base64 bugs to bug-coreutils@gnu.org
 GNU coreutils home page: <http://www.gnu.org/software/coreutils/>
 General help using GNU software: <http://www.gnu.org/gethelp/>
 For complete documentation, run: info coreutils 'base64 invocation'
-"""
-            )
-            self.exit()
-            return
+""")
+                self.exit()
+                return
+            elif opt[0] == '--version':
+                self.write("""base64 (GNU coreutils) 8.21
+Copyright (C) 2013 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
 
-        if '-d' in self.args:
-            self.mode = 'd'
-            self.args.remove('-d')
+Written by Simon Josefsson.
+""")
+                self.exit()
+                return
+            elif opt[0] == '-d' or opt[0] == '--decode':
+                self.mode = 'd'
 
-        if '--decode' in self.args:
-            self.mode = 'd'
-            self.args.remove('--decode')
+            elif opt[0] == '-i' or opt[0] == '--ignore-garbage':
+                self.ignore = True
 
-        if '-i' in self.args:
-            self.ignore = True
-            self.args.remove('-i')
-
-        if '--ignore-garbage' in self.args:
-            self.ignore = True
-            self.args.remove('--ignore-garbage')
-
-        if '-w' in self.args:
-            self.args.remove(self.args[self.args.index('-w')+1])
-            self.args.remove('-w')
+            elif opt[0] == '-w' or opt[0] == 'wrap':
+                pass
 
         if self.input_data:
             self.dojob(self.input_data)
         else:
-            if len(self.args) > 1:
+            if len(args) > 1:
                 self.errorWrite(
                     """base64: extra operand '%s'
 Try 'base64 --help' for more information.
-""" % self.args[0]
-                )
+""" % args[0])
                 self.exit()
                 return
 
-            pname = self.fs.resolve_path(self.args[0], self.protocol.cwd)
+            pname = self.fs.resolve_path(args[0], self.protocol.cwd)
             if not self.fs.isdir(pname):
                 try:
                     self.dojob(self.fs.file_contents(pname))
                 except Exception as e:
                     print(str(e))
-                    self.errorWrite('base64: {}: No such file or directory\n'.format(self.args[0]))
+                    self.errorWrite('base64: {}: No such file or directory\n'.format(args[0]))
             else:
                 self.errorWrite('base64: read error: Is a directory\n')
 
