232d67c616f27edcae3e4eb42a20cee0e9296799
gglchrm <64428082+gglchrm@users.noreply.github.com>
2021-12-15 18:02:52 +0300

    #3

--- src/cowrie/commands/etc_init_d.py
+++ src/cowrie/commands/etc_init_d.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
-
 from cowrie.shell.command import HoneyPotCommand
 
 commands = {}
 
+
 class Command_etc_init_d(HoneyPotCommand):
-
-
     def not_found(self):
         output = "bash: /etc/init.d/iptables: No such file or directory"
         self.write(output + "\n")
@@ -17,4 +15,3 @@ class Command_etc_init_d(HoneyPotCommand):
 
 
 commands["/etc/init.d/iptables"] = Command_etc_init_d
-
